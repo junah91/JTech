87fc227860992aba2ec15573226bf59d9956b7d2
junah <junahindig@gmail.com>
2025-05-09 13:13:12 +0800

    jtech7

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: 'https://jt-ech.vercel.app/landing.html' }));
 
 const mongoURI = process.env.DB_URI;
 
